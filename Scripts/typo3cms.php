70c990fae65bca0046ca29e7dc5cac3e3a410984
Helmut Hummel <helmut.hummel@typo3.org>
2014-06-15 20:26:43 +0200

    [TASK] Use flexible and less error prone path detection

--- Scripts/typo3cms.php
+++ Scripts/typo3cms.php
@@ -1,27 +1,40 @@
 <?php
 
-$__boot = function() {
+call_user_func(function($scriptLocation) {
+	// Cleanup pollution
+	unset($GLOBALS['__self_dir']);
+
+	/**
+	 * Find out web path by trying several strategies
+	 */
+	if (getenv('TYPO3_PATH_WEB')) {
+		$webRoot = getenv('TYPO3_PATH_WEB');
+	} elseif (isset($scriptLocation)) {
+		$webRoot = $scriptLocation;
+	} elseif(file_exists(getcwd() . '/typo3cms') || file_exists(getcwd() . '/typo3cms.bat')) {
+		$webRoot = getcwd();
+	} else {
+		$webRoot = realpath(__DIR__ . '/../../../../');
+	}
+	define('PATH_site', strtr($webRoot, '\\', '/') . '/');
+
 	if (isset($_SERVER['argv'][1]) && $_SERVER['argv'][1] === 'legacy') {
+		// Legacy handling is subject to be removed ...
 		$argv0 = './typo3/cli_dispatch.phpsh';
-		$pwd = __DIR__ . '/../../../../typo3';
+		$pwd = PATH_site . 'typo3';
 		$_SERVER['PHP_SELF'] = $pwd;
-		chdir($pwd);
 		$_SERVER['PHP_SELF'] =
 		$_SERVER['PATH_TRANSLATED'] =
 		$_SERVER['SCRIPT_FILENAME'] =
 		$_SERVER['SCRIPT_NAME'] = $argv0;
 		$_SERVER['argv'] = array_slice($_SERVER['argv'], 2);
 		array_unshift($_SERVER['argv'], $argv0);
-
-		define('PATH_site', realpath(__DIR__ . '/../../../../') . '/');
-		define('PATH_thisScript', realpath(__DIR__ . '/../../../../' . $argv0));
+		define('PATH_thisScript', realpath(PATH_site . $argv0));
+		chdir(PATH_site);
 		require __DIR__ . '/../Scripts/CliDispatch.php';
 		exit(0);
 	} else {
-		// TODO: If we find a more reliable way finding this path it would be nice
-		// Maybe add the possibiliy to get this from env
-		define('PATH_site', realpath(__DIR__ . '/../../../../') . '/');
-		define('PATH_thisScript', realpath(__DIR__ . '/../../../../typo3cms'));
+		define('PATH_thisScript', realpath(PATH_site . 'typo3cms'));
 	}
 
 	require PATH_site . 'typo3/sysext/core/Classes/Core/Bootstrap.php';
@@ -31,6 +44,5 @@ $__boot = function() {
 	$context = getenv('TYPO3_CONTEXT') ?: (getenv('REDIRECT_TYPO3_CONTEXT') ?: 'Production');
 	$bootstrap = new \Helhum\Typo3Console\Core\ConsoleBootstrap($context);
 	$bootstrap->run();
-};
 
-$__boot();
+}, (isset($__self_dir) ? $__self_dir : NULL));
