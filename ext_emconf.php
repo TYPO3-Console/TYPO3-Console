27123b304227a73ed1fc5ab772804ff4dd6cc37f
Helmut Hummel <helmut.hummel@typo3.org>
2015-09-04 12:12:26 +0200

    Use auto-generated output for ext_emconf.php and composer.json

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,34 +1,42 @@
 <?php
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'TYPO3 Console',
-	'description' => 'A reliable and powerful command line interface for TYPO3 CMS',
-	'category' => 'cli',
-	'shy' => 1,
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => 'mod',
-	'state' => 'stable',
-	'internal' => 0,
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
-	'lockType' => '',
-	'author' => 'Helmut Hummel',
-	'author_email' => 'helmut.hummel@typo3.org',
-	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'version' => '1.2.0',
-	'_md5_values_when_last_written' => '',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '7.4.0-7.99.99',
-		),
-		'conflicts' => array(),
-		'suggests' => array()
-	),
-	'suggests' => array()
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'TYPO3 Console',
+  'description' => 'A reliable and powerful command line interface for TYPO3 CMS',
+  'category' => 'cli',
+  'shy' => 1,
+  'dependencies' => '',
+  'conflicts' => '',
+  'priority' => '',
+  'loadOrder' => '',
+  'module' => 'mod',
+  'state' => 'stable',
+  'internal' => 0,
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'modify_tables' => '',
+  'clearCacheOnLoad' => 0,
+  'lockType' => '',
+  'author' => 'Helmut Hummel',
+  'author_email' => 'helmut.hummel@typo3.org',
+  'author_company' => '',
+  'CGLcompliance' => '',
+  'CGLcompliance_note' => '',
+  'version' => '1.2.0',
+  '_md5_values_when_last_written' => '',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '7.4.0-7.99.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  'suggests' => 
+  array (
+  ),
 );
