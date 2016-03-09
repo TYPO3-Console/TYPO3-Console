97294a45b1a006d183fa167e7e7a6285f0e5626a
Helmut Hummel <info@helhum.io>
2016-03-09 23:37:07 +0100

    Remove unnecessary run level config
    run level full is now default

--- Classes/Package.php
+++ Classes/Package.php
@@ -86,16 +86,11 @@ class Package extends \TYPO3\CMS\Core\Package\Package
 
         $bootstrap->setRunLevelForCommand('typo3_console:install:databasedata', RunLevel::LEVEL_MINIMAL);
         $bootstrap->addBootingStepForCommand('typo3_console:install:databasedata', 'helhum.typo3console:database');
-        $bootstrap->setRunLevelForCommand('typo3_console:install:defaultconfiguration', RunLevel::LEVEL_FULL);
         $bootstrap->setRunLevelForCommand('typo3_console:install:*', RunLevel::LEVEL_COMPILE);
 
         $bootstrap->setRunLevelForCommand('typo3_console:cache:flush', RunLevel::LEVEL_COMPILE);
         $bootstrap->addBootingStepForCommand('typo3_console:cache:flush', 'helhum.typo3console:database');
-        $bootstrap->setRunLevelForCommand('typo3_console:cache:*', RunLevel::LEVEL_FULL);
 
         $bootstrap->setRunLevelForCommand('typo3_console:backend:*', RunLevel::LEVEL_MINIMAL);
-        $bootstrap->setRunLevelForCommand('typo3_console:documentation:*', RunLevel::LEVEL_FULL);
-        $bootstrap->setRunLevelForCommand('typo3_console:scheduler:*', RunLevel::LEVEL_FULL);
-        $bootstrap->setRunLevelForCommand('typo3_console:cleanup:*', RunLevel::LEVEL_FULL);
     }
 }
