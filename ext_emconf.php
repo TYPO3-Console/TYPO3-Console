c02c057f2f4b6ff3981e91823636d788f2c5ec1d
Helmut Hummel <info@helhum.io>
2016-03-10 19:12:49 +0100

    Release version 1.2.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'author_company' => '',
   'CGLcompliance' => '',
   'CGLcompliance_note' => '',
-  'version' => '1.2.5',
+  'version' => '1.2.6',
   'constraints' => 
   array (
     'depends' => 
