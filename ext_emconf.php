8da0cd556ff49b261478d9fe975048ecd854ba26
Helmut Hummel <info@helhum.io>
2016-03-23 19:42:18 +0100

    [TASK] Fix TYPO3 dependency in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '7.6.0-7.6.99',
+      'typo3' => '7.6.0-8.0.99',
     ),
     'conflicts' => 
     array (
