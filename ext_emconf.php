80eae16536490f3913658abf208f702427d661b5
Helmut Hummel <info@helhum.io>
2016-04-03 18:16:45 +0200

    Make master officially compatible with current TYPO3 master

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '7.6.0-8.0.99',
+      'typo3' => '7.6.0-8.1.99',
     ),
     'conflicts' => 
     array (
