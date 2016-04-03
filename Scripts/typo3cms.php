3ead0f89ea1c076d483df9dde0d8a7d17a8a0fde
Helmut Hummel <info@helhum.io>
2016-04-03 17:43:16 +0200

    Further cleanup entry scripts
    This also fixes the ugliness of a fatal (instead of nice error message)
    in case the environment was not set up correctly.

--- Scripts/typo3cms.php
+++ Scripts/typo3cms.php
@@ -1,13 +1,18 @@
 <?php
 call_user_func(function () {
-    if (getenv('TYPO3_PATH_WEB')) {
-        $webRoot = getenv('TYPO3_PATH_WEB');
-    } else {
-        echo "Could not find TYPO3 installation root path! Make sure TYPO3_PATH_WEB environment variable is set correctly and your typo3cms binary is up to date!\n";
+    // The following checks are safeguards to identify issues with outdated typo3cms binaries
+    // Check again if php requirement is satisfied.
+    if (version_compare(PHP_VERSION, '5.5.0', '<')) {
+        echo 'This version of TYPO3 Console requires PHP 5.5.0 or above!' . PHP_EOL;
+        exit(1);
+    }
+    // Check again if path is set correctly.
+    if (!getenv('TYPO3_PATH_WEB')) {
+        echo 'Could not find TYPO3 installation root path! Make sure TYPO3_PATH_WEB environment variable is set correctly and your typo3cms binary is up to date!' . PHP_EOL;
         exit(1);
     }
 
-    define('PATH_site', strtr($webRoot, '\\', '/') . '/');
+    define('PATH_site', strtr(getenv('TYPO3_PATH_WEB'), '\\', '/') . '/');
     define('PATH_thisScript', realpath(PATH_site . 'typo3/cli_dispatch.phpsh'));
 
     $classLoader = require_once realpath(PATH_site . 'typo3/../') . '/vendor/autoload.php';
