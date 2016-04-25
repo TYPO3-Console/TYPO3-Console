8299f7811581159890c67ede0c44d527468d3a37
Helmut Hummel <info@helhum.io>
2016-04-25 17:30:30 +0200

    Update extension version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'author_company' => '',
   'CGLcompliance' => '',
   'CGLcompliance_note' => '',
-  'version' => '2.0.0',
+  'version' => '3.0.0',
   'constraints' => 
   array (
     'depends' => 
