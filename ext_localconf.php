27181ae819d8c1e914d5a2eea712e69930711838
Helmut Hummel <info@helhum.io>
2016-07-03 14:55:49 +0200

    [TASK] Allow array arguments in low level commands
    The type converter must be registered much earlier
    in the bootstrap.
    
    This change requires clearing the cache once after update.

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,9 +2,6 @@
 defined('TYPO3_MODE') or die('Access denied.');
 
 call_user_func(function ($extensionKey) {
-    if ((TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_CLI)) {
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter(\Helhum\Typo3Console\Property\TypeConverter\ArrayConverter::class);
-    }
     if ((TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_CLI) || (TYPO3_MODE === 'BE' && isset($_GET['M']) && 'tools_ExtensionmanagerExtensionmanager' === $_GET['M'])) {
         $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
         $signalSlotDispatcher->connect(
