76801258c19e24479fd8cdf68fe9245aef7deee1
Helmut Hummel <info@helhum.io>
2016-07-07 10:10:07 +0200

    [TASK] Raise version number to 3.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = array (
   'author' => 'Helmut Hummel',
   'author_email' => 'info@helhum.io',
   'author_company' => 'helhum.io',
-  'version' => '3.0.0',
+  'version' => '3.1.0',
   'constraints' => 
   array (
     'depends' => 
