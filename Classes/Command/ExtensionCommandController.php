312a972cd2a03bfb04667bdde59e40ab120538bb
Aljoscha Weber <Anubiso@web.de>
2016-07-13 10:25:35 +0200

    Also install dependencies with the desired extensions

--- Classes/Command/ExtensionCommandController.php
+++ Classes/Command/ExtensionCommandController.php
@@ -37,6 +37,18 @@ class ExtensionCommandController extends CommandController
      */
     protected $extensionInstaller;
 
+	/**
+     * @var \TYPO3\CMS\Extensionmanager\Utility\ExtensionModelUtility
+     * @inject
+     */
+    protected $extensionModelUtility;
+	
+	/**
+     * @var \TYPO3\CMS\Extensionmanager\Service\ExtensionManagementService
+	 * @inject
+     */
+    protected $managementService;
+
     /**
      * @var \TYPO3\CMS\Core\Package\PackageManager
      * @inject
@@ -55,11 +67,31 @@ class ExtensionCommandController extends CommandController
     public function activateCommand(array $extensionKeys)
     {
         $this->emitPackagesMayHaveChangedSignal();
+		$installedExtensions = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getLoadedExtensionListArray();
         foreach ($extensionKeys as $extensionKey) {
-            $this->extensionInstaller->install($extensionKey);
-        }
-        $extensionKeysAsString = implode('", "', $extensionKeys);
-        if (count($extensionKeys) === 1) {
+			try {
+				if (in_array($extensionKey, $installedExtensions)) {
+					continue; 
+				}
+				
+				$extension = $this->extensionModelUtility->mapExtensionArrayToModel(
+					$this->extensionInstaller->enrichExtensionWithDetails($extensionKey)
+				);
+				if ($this->managementService->installExtension($extension) === false) {
+					 $this->outputLine('<error>Extension "%s" could not be installed.</error>', [$extensionKey]);
+					 $this->sendAndExit(2);
+				}
+			}
+			catch (\TYPO3\CMS\Extensionmanager\Exception\ExtensionManagerException $e) {
+				$this->outputLine('<error>There was an exception: "%s".</error>', [$e->getMessage()]);
+				$this->sendAndExit(2);
+			} catch (\TYPO3\CMS\Core\Package\Exception\PackageStatesFileNotWritableException $e) {
+				$this->outputLine('<error>There was an exception: "%s".</error>', [$e->getMessage()]);
+				$this->sendAndExit(2);
+			}
+		}
+		
+		if (count($extensionKeys) === 1) {
             $this->outputLine('<info>Extension "%s" is now active.</info>', [$extensionKeysAsString]);
         } else {
             $this->outputLine('<info>Extensions "%s" are now active.</info>', [$extensionKeysAsString]);
