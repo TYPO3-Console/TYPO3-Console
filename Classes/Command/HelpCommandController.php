3b8da21a4f26e3e855c44ce86b6b778376562f76
Helmut Hummel <info@helhum.io>
2016-07-29 13:44:06 +0200

    Set version to 3.3.0

--- Classes/Command/HelpCommandController.php
+++ Classes/Command/HelpCommandController.php
@@ -30,7 +30,7 @@ class HelpCommandController extends CommandController
      *
      * @var string
      */
-    private $version = '3.2.3';
+    private $version = '3.3.0';
 
     /**
      * @var \Helhum\Typo3Console\Mvc\Cli\CommandManager
