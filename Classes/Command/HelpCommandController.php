74784c599e4b2b8fddcd18b20725b4b0de6769ac
Helmut Hummel <info@helhum.io>
2016-08-01 15:29:37 +0200

    Start 4.x development

--- Classes/Command/HelpCommandController.php
+++ Classes/Command/HelpCommandController.php
@@ -30,7 +30,7 @@ class HelpCommandController extends CommandController
      *
      * @var string
      */
-    private $version = '3.3.0';
+    private $version = '4.0.0';
 
     /**
      * @var \Helhum\Typo3Console\Mvc\Cli\CommandManager
