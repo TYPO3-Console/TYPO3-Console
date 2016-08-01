4910c6960351958366f69313cc15e9f6743afac9
Helmut Hummel <info@helhum.io>
2016-08-01 16:04:36 +0200

    [!!!][TASK] Remove extension files
    TYPO3 Console now is a composer package only.
    
    It is currently still possible to build the extension
    by doing "composer build-extension" after cloning the repo
    into typo3conf/ext/typo3_console

--- Resources/Private/ExtensionArtifacts/ext_localconf.php
+++ Resources/Private/ExtensionArtifacts/ext_localconf.php
@@ -2,11 +2,6 @@
 defined('TYPO3_MODE') or die('Access denied.');
 
 call_user_func(function () {
-    if (\TYPO3\CMS\Core\Core\Bootstrap::usesComposerClassLoading()) {
-        // Do not register this slot for binary copy in composer mode.
-        // The composer plugin takes care doing so in this case.
-        return;
-    }
     if ((TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_CLI) || (TYPO3_MODE === 'BE' && isset($_GET['M']) && 'tools_ExtensionmanagerExtensionmanager' === $_GET['M'])) {
         $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
         $signalSlotDispatcher->connect(
