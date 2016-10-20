8dd82053bcb55c0c5daa8d9dbdcc7afd9caa2dd1
Helmut Hummel <info@helhum.io>
2016-10-20 13:53:13 +0200

    [BUGFIX] Remove broken realurl workaround
    Especially with realurl 2.x available, the workaround cleared
    too many realurl tables, which may lead to a broken data state.
    
    Fixes #327
    Fixes #328

--- Classes/Service/CacheService.php
+++ Classes/Service/CacheService.php
@@ -160,7 +160,7 @@ class CacheService implements SingletonInterface
         $tables = $this->databaseConnection->admin_get_tables();
         foreach ($tables as $table) {
             $tableName = $table['Name'];
-            if (substr($tableName, 0, 3) === 'cf_' || ($tableName !== 'tx_realurl_redirects' && substr($tableName, 0, 11) === 'tx_realurl_')) {
+            if (strpos($tableName, 'cf_') === 0) {
                 $this->databaseConnection->exec_TRUNCATEquery($tableName);
             }
         }
