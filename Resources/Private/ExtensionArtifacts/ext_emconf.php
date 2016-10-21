bd32888c0f3588db44e6a54fdc49dd3250545a67
Helmut Hummel <typo3@helhum.io>
2016-10-21 22:17:55 +0200

    Revert "Release 3.5.0" (#333)
    Revert accidental dowgrade of version
    This reverts commit cc8c8604ccc6402af2aa9fb75c4af5b6eaedaa29.

--- Resources/Private/ExtensionArtifacts/ext_emconf.php
+++ Resources/Private/ExtensionArtifacts/ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = array(
   'author' => 'Helmut Hummel',
   'author_email' => 'info@helhum.io',
   'author_company' => 'helhum.io',
-  'version' => '3.5.0',
+  'version' => '4.0.0',
   'constraints' =>
   array(
     'depends' =>
