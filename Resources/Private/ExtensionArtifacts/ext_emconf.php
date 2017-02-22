80d47a3ec517790b49f253df1936ec3e1461ca60
Helmut Hummel <info@helhum.io>
2017-02-22 23:01:00 +0100

    Release 4.2.1

--- Resources/Private/ExtensionArtifacts/ext_emconf.php
+++ Resources/Private/ExtensionArtifacts/ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
   'author' => 'Helmut Hummel',
   'author_email' => 'info@helhum.io',
   'author_company' => 'helhum.io',
-  'version' => '4.2.0',
+  'version' => '4.2.1',
   'constraints' =>
   [
     'depends' =>
