0bf98421a5d775cf34655f5ad476f5d6f496a0e9
technid <madsjensen01@gmail.com>
2017-03-01 17:24:53 +0100

    Skip user authentication for database:updateschema command (#430)
    If we skip the user authentication, this command is executable with an empty database
    or missing be_users table.

--- Configuration/Console/Commands.php
+++ Configuration/Console/Commands.php
@@ -19,6 +19,7 @@ return [
         'typo3_console:commandreference:render' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_MINIMAL ,
         'typo3_console:configuration:*' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_MINIMAL ,
         'typo3_console:database:import' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_MINIMAL ,
+        'typo3_console:database:updateschema' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_MINIMAL ,
         'typo3_console:extension:*' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_FULL ,
         'typo3_console:extension:removeinactive' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_COMPILE ,
     ],
@@ -26,5 +27,9 @@ return [
         'typo3_console:install:databasedata' => ['helhum.typo3console:database'],
         'typo3_console:install:defaultconfiguration' => ['helhum.typo3console:database'],
         'typo3_console:cache:flush' => ['helhum.typo3console:database'],
+        'typo3_console:database:updateschema' => [
+            'helhum.typo3console:database',
+            'helhum.typo3console:persistence',
+        ],
     ]
 ];
