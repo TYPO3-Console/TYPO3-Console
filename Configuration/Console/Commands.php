91e73901fe9ec0780f2a21c7dc38b7ee3d9551ab
Helmut Hummel <typo3@helhum.io>
2017-03-02 10:13:13 +0100

    Cleanup command definition and CGL (#436)
    Command definition of extension:removeinactive is wrong
    and effectively uses full RunLevel. This is fine
    but we can then just skip the extra definition

--- Configuration/Console/Commands.php
+++ Configuration/Console/Commands.php
@@ -22,8 +22,6 @@ return [
         'typo3_console:configuration:*' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_MINIMAL,
         'typo3_console:database:import' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_MINIMAL,
         'typo3_console:database:updateschema' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_MINIMAL,
-        'typo3_console:extension:*' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_FULL,
-        'typo3_console:extension:removeinactive' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_COMPILE,
     ],
     'bootingSteps' => [
         'typo3_console:install:databasedata' => ['helhum.typo3console:database'],
