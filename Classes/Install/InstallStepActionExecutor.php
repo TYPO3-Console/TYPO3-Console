c0643739dd7828aaa20ba80581c9329c338311ed
Helmut Hummel <info@helhum.io>
2017-03-12 18:53:22 +0100

    Refactor installer
    * Remove "*needsexecution" commands and replace with one method
    * Make every action return a clean response object
    * Use this response object to check the state
    
    The result is cleaner code, a more reliable install process
    and better (error) messages for users.

---
+++ Classes/Install/InstallStepActionExecutor.php
@@ -0,0 +1,92 @@
+<?php
+namespace Helhum\Typo3Console\Install;
+
+/*
+ * This file is part of the TYPO3 console project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read
+ * LICENSE file that was distributed with this source code.
+ *
+ */
+
+use TYPO3\CMS\Extbase\Object\ObjectManager;
+use TYPO3\CMS\Install\Controller\Action\ActionInterface;
+use TYPO3\CMS\Install\Controller\Action\Step\StepInterface;
+use TYPO3\CMS\Install\Controller\Exception\RedirectException;
+
+/**
+ * This class is responsible for properly creating install tool step actions
+ * and executing them if needed
+ */
+class InstallStepActionExecutor
+{
+    /**
+     * @var ObjectManager
+     */
+    private $objectManager;
+
+    /**
+     * @param ObjectManager $objectManager
+     */
+    public function __construct(ObjectManager $objectManager)
+    {
+        // @deprecated Object Manager can be removed, once TYPO3 7.6 support is removed
+        $this->objectManager = $objectManager;
+    }
+
+    /**
+     * Executes the given action and returns their response messages
+     *
+     * @param string $actionName Name of the install step
+     * @param array $arguments Arguments for the install step
+     * @param bool $dryRun If true, do not execute the action, but only check if execution is necessary
+     * @return InstallStepResponse
+     */
+    public function executeActionWithArguments($actionName, array $arguments = [], $dryRun = false)
+    {
+        return $this->executeAction($this->createActionWithNameAndArguments($actionName, $arguments), $dryRun);
+    }
+
+    /**
+     * @param string $actionName
+     * @param array $arguments
+     * @return StepInterface|ActionInterface
+     */
+    private function createActionWithNameAndArguments($actionName, array $arguments = [])
+    {
+        $classPrefix = 'TYPO3\\CMS\\Install\\Controller\\Action\\Step\\';
+        $className = $classPrefix . ucfirst($actionName);
+
+        /** @var StepInterface|ActionInterface $action */
+        $action = $this->objectManager->get($className);
+        $action->setController('step');
+        $action->setAction($actionName);
+        $action->setPostValues(['values' => $arguments]);
+
+        return $action;
+    }
+
+    /**
+     * @param StepInterface $action
+     * @param bool $dryRun
+     * @return InstallStepResponse
+     */
+    private function executeAction(StepInterface $action, $dryRun = false)
+    {
+        $messages = [];
+        try {
+            $needsExecution = $action->needsExecution();
+        } catch (RedirectException $e) {
+            return new InstallStepResponse(true, $messages, true);
+        }
+        if ($needsExecution && !$dryRun) {
+            $messages = $action->execute();
+            $needsExecution = false;
+        }
+        return new InstallStepResponse($needsExecution, $messages);
+    }
+}
---
+++ Classes/Install/InstallStepResponse.php
@@ -0,0 +1,73 @@
+<?php
+namespace Helhum\Typo3Console\Install;
+
+/*
+ * This file is part of the TYPO3 console project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read
+ * LICENSE file that was distributed with this source code.
+ *
+ */
+
+use TYPO3\CMS\Install\Status\StatusInterface;
+
+/**
+ * Response of an install step
+ */
+class InstallStepResponse
+{
+    /**
+     * @var bool
+     */
+    private $actionNeedsExecution;
+
+    /**
+     * @var StatusInterface[]
+     */
+    private $messages = [];
+
+    /**
+     * @var bool
+     */
+    private $actionNeedsReevaluation;
+
+    /**
+     * @param bool $actionNeedsExecution
+     * @param StatusInterface[] $messages
+     * @param bool $actionNeedsReevaluation
+     */
+    public function __construct($actionNeedsExecution, array $messages, $actionNeedsReevaluation = false)
+    {
+        $this->actionNeedsExecution = (bool)$actionNeedsExecution;
+        $this->messages = $messages;
+        $this->actionNeedsReevaluation = (bool)$actionNeedsReevaluation;
+    }
+
+    /**
+     * @return bool
+     */
+    public function actionNeedsExecution()
+    {
+        return $this->actionNeedsExecution;
+    }
+
+    /**
+     * @return StatusInterface[]
+     */
+    public function getMessages()
+    {
+        return $this->messages;
+    }
+
+    /**
+     * @return bool
+     */
+    public function actionNeedsReevaluation()
+    {
+        return $this->actionNeedsReevaluation;
+    }
+}
