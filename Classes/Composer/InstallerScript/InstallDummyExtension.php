d4490788f48fcf6214d8cdb20dd9d795dcd5341d
Christian Wolff <chris@connye.com>
2017-04-13 12:39:34 +0200

    Add composer command for config change
    This adds the composer command for changing the composer.json directly to the warning message 
    this should make it easier for developer to update.

--- Classes/Composer/InstallerScript/InstallDummyExtension.php
+++ Classes/Composer/InstallerScript/InstallDummyExtension.php
@@ -56,6 +56,8 @@ class InstallDummyExtension implements InstallerScriptInterface
             $io->writeError('<warning>Installation of TYPO3 extension has been deprecated</warning>');
             $io->writeError('<warning>To get rid of this message, set "install-extension-dummy" option to false</warning>');
             $io->writeError('<warning>in "extra -> helhum/typo3-console" section of root composer.json</warning>');
+            $io->writeError('<warning>you might use the following command to set the parameter:</warning>');
+            $io->writeError('<warning>composer config extra.helhum/typo3-console.install-extension-dummy false</warning>');
 
             $extResourcesDir = __DIR__ . '/../../../Resources/Private/ExtensionArtifacts';
             $resources = [
