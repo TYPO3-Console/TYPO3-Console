ef8fb8dd9d548c29916ccd275367961562036723
Helmut Hummel <info@helhum.io>
2017-09-11 14:40:38 +0200

    [BUGFIX] Throw exception on failed cache clearing
    When flushing caches, we let the sub command fail
    in case there are errors

--- Classes/Command/CacheCommandController.php
+++ Classes/Command/CacheCommandController.php
@@ -58,7 +58,6 @@ class CacheCommandController extends CommandController
         if (!$filesOnly) {
             try {
                 $this->cacheService->flush($force);
-                $this->commandDispatcher->executeCommand('cache:flushcomplete');
             } catch (\Throwable $e) {
                 $exitCode = 1;
                 $filesOnly = true;
@@ -67,6 +66,7 @@ class CacheCommandController extends CommandController
                 $exitCode = 1;
                 $filesOnly = true;
             }
+            $this->commandDispatcher->executeCommand('cache:flushcomplete');
         }
         if ($filesOnly) {
             $this->cacheService->flushFileCaches($force);
