a11c6f87f7e95ceea716574ce5280f26ae488649
Helmut Hummel <info@helhum.io>
2017-09-11 23:53:27 +0200

    Release 4.8.1

--- Classes/Command/HelpCommandController.php
+++ Classes/Command/HelpCommandController.php
@@ -30,7 +30,7 @@ class HelpCommandController extends CommandController
      *
      * @var string
      */
-    private $version = '4.8.0';
+    private $version = '4.8.1';
 
     /**
      * @var \Helhum\Typo3Console\Mvc\Cli\CommandManager
