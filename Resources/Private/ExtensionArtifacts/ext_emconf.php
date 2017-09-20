05223c0d7ed92059a8cc078323624c7cdcf36ae2
Helmut Hummel <typo3@helhum.io>
2017-09-20 16:20:42 +0200

    [!!!] Remove PHP 5 support (#570)
    Fixes #569

--- Resources/Private/ExtensionArtifacts/ext_emconf.php
+++ Resources/Private/ExtensionArtifacts/ext_emconf.php
@@ -14,6 +14,7 @@ $EM_CONF[$_EXTKEY] = [
   'version' => '5.0.0',
   'constraints' => [
     'depends' => [
+      'php' => '7.0.0-7.1.99',
       'typo3' => '7.6.0-8.7.99',
     ],
     'conflicts' => [
