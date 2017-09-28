4bee0c615cec54dd24f9b650818156bf73ff330e
Helmut Hummel <info@helhum.io>
2017-09-28 23:59:40 +0200

    [BUGFIX] Fix console as extension

--- Scripts/typo3cms.php
+++ Scripts/typo3cms.php
@@ -10,15 +10,7 @@
         // Console is extension, but TYPO3_PATH_ROOT was not set, because binary is symlinked, so set it here
         putenv('TYPO3_PATH_ROOT=' . $rootPath);
         $classLoader = require $autoLoadFile;
-    } elseif (file_exists($autoLoadFile = realpath(getenv('TYPO3_PATH_ROOT') . '/typo3') . '/../vendor/autoload.php')) {
-        // Console is extension, this TYPO3_PATH_ROOT was set in typo3cms script, which is located in TYPO3 root
-        $classLoader = require $autoLoadFile;
     } else {
-        if (file_exists(realpath(getenv('TYPO3_PATH_WEB') . '/typo3') . '/../vendor/autoload.php')) {
-            // Console is extension, this TYPO3_PATH_WEB was set in outdated typo3cms script, which is located in TYPO3 root
-            echo 'You seem to use an outdated typo3cms binary in your TYPO3 root directory.' . PHP_EOL;
-            echo 'Please properly install typo3_console in the TYPO3 Extension Manager.' . PHP_EOL;
-        }
         echo 'Could not find autoload.php file. Is TYPO3_PATH_ROOT specified correctly?' . PHP_EOL;
         exit(1);
     }
