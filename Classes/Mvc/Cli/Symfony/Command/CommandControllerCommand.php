149206e8d4d8e57a8fdf6d0fdd4b0840b5461a14
Helmut Hummel <info@helhum.io>
2017-10-03 02:12:54 +0200

    Centralize input definition creation

--- Classes/Mvc/Cli/Symfony/Command/CommandControllerCommand.php
+++ Classes/Mvc/Cli/Symfony/Command/CommandControllerCommand.php
@@ -31,10 +31,7 @@ use Helhum\Typo3Console\Mvc\Cli\RequestHandler;
 use Helhum\Typo3Console\Mvc\Cli\Symfony\Application;
 use Symfony\Component\Console\Application as BaseApplication;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -94,92 +91,11 @@ class CommandControllerCommand extends Command
     {
         $this->setDescription($this->commandDefinition->getShortDescription());
         $this->setHelp($this->commandDefinition->getDescription());
-
         $strict = $this->commandDefinition->shouldValidateInputStrict();
         if (!$strict) {
-            // @deprecated in 5.0 will be removed in 6.0
             $this->ignoreValidationErrors();
         }
-        $applicationDefinition = $this->getApplicationDefinition();
-        foreach ($this->commandDefinition->getArguments() as $argument) {
-            $argumentName = $argument->getName();
-            if (!$strict && $applicationDefinition->hasArgument($argumentName)) {
-                // That is most likely the "command" argument
-                // We append the full command identifier so we don't get an exception from Symfony
-                $argumentName .= '_' . $this->commandDefinition->getCommandIdentifier();
-            }
-            $this->addArgument(
-                $argumentName,
-                $strict && $argument->isRequired() ? InputArgument::REQUIRED : InputArgument::OPTIONAL,
-                $argument->getDescription()
-            );
-
-            if ($strict) {
-                continue;
-            }
-            // Fallback option for compatibility but only in case no other option with same name is defined already
-            // @deprecated in 5.0 will be removed in 6.0
-            if ($applicationDefinition->hasOption($argument->getOptionName())) {
-                // Option is globally defined already, so we can skip here
-                continue;
-            }
-            $this->addOption(
-                $argument->getOptionName(),
-                null,
-                InputOption::VALUE_REQUIRED,
-                $argument->getDescription()
-            );
-        }
-        foreach ($this->commandDefinition->getOptions() as $option) {
-            if ($applicationDefinition->hasOption($option->getOptionName())) {
-                // Option is globally defined already, so we can skip here
-                continue;
-            }
-            if (!$option->acceptsValue()) {
-                $this->addOption(
-                    $option->getOptionName(),
-                    null,
-                    // VALUE_OPTIONAL for compatibility
-                    // @deprecated in 5.0 will be changed to VALUE_NONE in 6.0
-                    $strict ? InputOption::VALUE_NONE : InputOption::VALUE_OPTIONAL,
-                    $option->getDescription()
-                );
-            } else {
-                $this->addOption(
-                    $option->getOptionName(),
-                    null,
-                    InputOption::VALUE_REQUIRED,
-                    $option->getDescription(),
-                    $option->getDefaultValue()
-                );
-            }
-
-            if ($strict) {
-                continue;
-            }
-            // Fallback to argument for compatibility
-            // @deprecated in 5.0 will be removed in 6.0
-            $this->addArgument(
-                $option->getName(),
-                InputArgument::OPTIONAL,
-                $option->getDescription(),
-                $option->getDefaultValue()
-            );
-        }
-    }
-
-    /**
-     * During configure, we don't have an application object yet,
-     * so we create a fake one, just to get the default definitions.
-     *
-     * @return InputDefinition
-     */
-    private function getApplicationDefinition()
-    {
-        if ($this->application === null) {
-            return (new Application())->getDefinition();
-        }
-        return $this->getApplication()->getDefinition();
+        $this->setDefinition($this->commandDefinition->getInputDefinitions());
     }
 
     /**
