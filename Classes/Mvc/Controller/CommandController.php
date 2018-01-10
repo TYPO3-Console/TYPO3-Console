54dd425bc601b6adcf9709247610118a9ca82861
Helmut Hummel <info@helhum.io>
2018-01-10 13:45:14 +0100

    [BUGFIX] Output deprecation message to stderr
    To not interfere with re-using the output of command
    message returns, the deprecation message is now
    sent to stderr

--- Classes/Mvc/Controller/CommandController.php
+++ Classes/Mvc/Controller/CommandController.php
@@ -261,8 +261,8 @@ abstract class CommandController implements CommandControllerInterface
         $commandResult = $this->{$this->commandMethodName}(...$preparedArguments);
         if ($commandResult !== null) {
             $this->outputLine((string)$commandResult);
-            $this->outputLine('<warning>Returning a string from a command method is deprecated.</warning>');
-            $this->outputLine('<warning>Please use $this->outputLine() instead.</warning>');
+            $this->output->getSymfonyConsoleOutput()->getErrorOutput()->writeln('<warning>Returning a string from a command method is deprecated.</warning>');
+            $this->output->getSymfonyConsoleOutput()->getErrorOutput()->writeln('<warning>Please use $this->outputLine() instead.</warning>');
         }
     }
 
