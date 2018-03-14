3b151c37ec7bac87c95dfc6738c8b04757e306a4
Helmut Hummel <info@helhum.io>
2018-03-15 00:46:52 +0100

    [TASK] Add code coverage for sonar

--- Classes/Console/Mvc/Cli/Symfony/Input/ArgvInput.php
+++ Classes/Console/Mvc/Cli/Symfony/Input/ArgvInput.php
@@ -2,25 +2,17 @@
 declare(strict_types=1);
 namespace Helhum\Typo3Console\Mvc\Cli\Symfony\Input;
 
-/***************************************************************
- *  Copyright notice
+/*
+ * This file is part of the TYPO3 Console project.
  *
- *  (c) 2017 Helmut Hummel <info@helhum.io>
- *  All rights reserved
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *  A copy is found in the text file GPL.txt and important notices to the license
- *  from the author is found in LICENSE.txt distributed with these scripts.
+ * For the full copyright and license information, please read
+ * LICENSE file that was distributed with this source code.
  *
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ */
 
 class ArgvInput extends \Symfony\Component\Console\Input\ArgvInput
 {
