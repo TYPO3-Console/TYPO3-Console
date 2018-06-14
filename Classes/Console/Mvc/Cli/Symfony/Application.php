b53d349ee052b1a0ef776891d8928bc4f0b7ad6a
Helmut Hummel <info@helhum.io>
2018-06-14 19:46:06 +0200

    Release 5.3.2

--- Classes/Console/Mvc/Cli/Symfony/Application.php
+++ Classes/Console/Mvc/Cli/Symfony/Application.php
@@ -34,7 +34,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class Application extends BaseApplication
 {
-    const TYPO3_CONSOLE_VERSION = '5.3.1';
+    const TYPO3_CONSOLE_VERSION = '5.3.2';
     const COMMAND_NAME = 'typo3cms';
 
     /**
