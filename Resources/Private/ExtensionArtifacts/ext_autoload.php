da1ba9654220c0f355aa44456df43b2999f457e2
Helmut Hummel <info@helhum.io>
2018-07-02 15:32:51 +0200

    Further simplify extension build and release

--- Resources/Private/ExtensionArtifacts/ext_autoload.php
+++ Resources/Private/ExtensionArtifacts/ext_autoload.php
@@ -10,20 +10,7 @@ return (function () {
     $typo3AutoLoadFile = realpath(($rootPath = dirname(__DIR__, 3)) . '/typo3') . '/../vendor/autoload.php';
     putenv('TYPO3_PATH_ROOT=' . $rootPath);
     $classLoader = require $typo3AutoLoadFile;
-
-    $extensionBaseDir = __DIR__;
-    $autoloadDefinition = json_decode(file_get_contents($extensionBaseDir . '/composer.json'), true)['autoload']['psr-4'];
-    foreach ($autoloadDefinition as $prefix => $paths) {
-        $paths = array_map(
-            function ($path) use ($extensionBaseDir) {
-                return $extensionBaseDir . '/' . $path;
-            },
-            (array)$paths
-        );
-        $classLoader->addPsr4($prefix, $paths);
-    }
-    $pharFile = __DIR__ . '/Libraries/symfony-process.phar';
-    require 'phar://' . $pharFile . '/vendor/autoload.php';
+    require __DIR__ . '/Libraries/vendor/autoload.php';
 
     return $classLoader;
 })();
