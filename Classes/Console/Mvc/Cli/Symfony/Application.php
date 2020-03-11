2acb072b7accdb367ce97760bd511472608851dd
Helmut Hummel <info@helhum.io>
2020-03-11 15:19:16 +0100

    Release 6.0.0-alpha1

--- Classes/Console/Mvc/Cli/Symfony/Application.php
+++ Classes/Console/Mvc/Cli/Symfony/Application.php
@@ -37,7 +37,7 @@ use Symfony\Component\Console\Style\SymfonyStyle;
  */
 class Application extends BaseApplication
 {
-    const TYPO3_CONSOLE_VERSION = '5.8.3';
+    const TYPO3_CONSOLE_VERSION = '6.0.0';
     const COMMAND_NAME = 'typo3cms';
 
     /**
