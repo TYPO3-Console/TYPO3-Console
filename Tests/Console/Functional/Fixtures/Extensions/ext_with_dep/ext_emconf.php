fff9472a38141862645d481b1b5c115fbb81ae26
Helmut Hummel <info@helhum.io>
2020-03-17 12:55:47 +0100

    [BUGFIX] Fix generating PackageStates.php
    Fixes: #877

---
+++ Tests/Console/Functional/Fixtures/Extensions/ext_with_dep/ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+  'title' => 'Test Extension with dependency',
+  'description' => 'Extension fixture to prove generating package states working correctly',
+  'category' => 'cli',
+  'state' => 'stable',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'modify_tables' => '',
+  'clearCacheOnLoad' => 0,
+  'author' => 'Helmut Hummel',
+  'author_email' => 'info@helhum.io',
+  'author_company' => 'helhum.io',
+  'version' => '0.1.0',
+  'constraints' => [
+    'depends' => [
+      'typo3' => '4.5.0-',
+      'ext_no_dep' => '',
+    ],
+    'conflicts' => [
+    ],
+    'suggests' => [
+    ],
+  ],
+];
