448a2ac57b9bb2f63af84256e7e84441e83fe1a9
Helmut Hummel <info@helhum.io>
2020-04-23 11:35:12 +0200

    Release 5.8.5

--- Classes/Console/Mvc/Cli/Symfony/Application.php
+++ Classes/Console/Mvc/Cli/Symfony/Application.php
@@ -37,7 +37,7 @@ use Symfony\Component\Console\Style\SymfonyStyle;
  */
 class Application extends BaseApplication
 {
-    const TYPO3_CONSOLE_VERSION = '5.8.4';
+    const TYPO3_CONSOLE_VERSION = '5.8.5';
     const COMMAND_NAME = 'typo3cms';
 
     /**
