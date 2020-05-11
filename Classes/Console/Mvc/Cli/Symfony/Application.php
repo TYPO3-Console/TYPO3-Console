43b72dc44334e298e9e4436bd1efe1ea53832576
Helmut Hummel <info@helhum.io>
2020-05-11 22:44:36 +0200

    Release 6.3.0

--- Classes/Console/Mvc/Cli/Symfony/Application.php
+++ Classes/Console/Mvc/Cli/Symfony/Application.php
@@ -41,7 +41,7 @@ use TYPO3\CMS\Core\Information\Typo3Version;
  */
 class Application extends BaseApplication
 {
-    const TYPO3_CONSOLE_VERSION = '6.2.1';
+    const TYPO3_CONSOLE_VERSION = '6.3.0';
     const COMMAND_NAME = 'typo3cms';
 
     /**
