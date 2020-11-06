471b5c9bb273a31bd67504298bbac338590fb73a
Helmut Hummel <info@helhum.io>
2020-11-06 12:29:45 +0100

    [BUGFIX] Make installer script Composer 2 compatible
    Fixes #941

--- Classes/Console/Composer/InstallerScripts.php
+++ Classes/Console/Composer/InstallerScripts.php
@@ -15,7 +15,6 @@ namespace Helhum\Typo3Console\Composer;
  */
 
 use Composer\Script\Event;
-use Composer\Semver\Constraint\EmptyConstraint;
 use Helhum\Typo3Console\Composer\InstallerScript\PopulateCommandConfiguration;
 use TYPO3\CMS\Composer\Plugin\Core\InstallerScripts\AutoloadConnector;
 use TYPO3\CMS\Composer\Plugin\Core\InstallerScripts\WebDirectory;
@@ -40,7 +39,7 @@ class InstallerScripts implements InstallerScriptsRegistration
         $scriptDispatcher->addInstallerScript(new PopulateCommandConfiguration(), 70);
         if (!class_exists(\TYPO3\CMS\Core\Composer\InstallerScripts::class)
             && !class_exists(\Helhum\Typo3ComposerSetup\Composer\InstallerScripts::class)
-            && $event->getComposer()->getRepositoryManager()->getLocalRepository()->findPackage('typo3/cms', new EmptyConstraint()) !== null
+            && $event->getComposer()->getRepositoryManager()->getLocalRepository()->findPackage('typo3/cms', '*') !== null
         ) {
             // @deprecated can be removed once TYPO3 8 support is removed
             $scriptDispatcher->addInstallerScript(new WebDirectory());
