ac2701434405cfebed9d75e0e22ae0bb330eafe0
Stephan Jorek <stephan.jorek@brandung.de>
2021-02-16 10:08:05 +0100

    [BUGFIX] escape database-username and -password in mysql-command

--- Classes/Console/Database/Process/MysqlCommand.php
+++ Classes/Console/Database/Process/MysqlCommand.php
@@ -141,10 +141,10 @@ class MysqlCommand
         $userDefinition = '';
         $passwordDefinition = '';
         if (!empty($this->dbConfig['user'])) {
-            $userDefinition = sprintf('user="%s"', $this->dbConfig['user']);
+            $userDefinition = sprintf('user="%s"', addslashes($this->dbConfig['user']));
         }
         if (!empty($this->dbConfig['password'])) {
-            $passwordDefinition = sprintf('password="%s"', $this->dbConfig['password']);
+            $passwordDefinition = sprintf('password="%s"', addslashes($this->dbConfig['password']));
         }
         $confFileContent = <<<EOF
 [mysqldump]
