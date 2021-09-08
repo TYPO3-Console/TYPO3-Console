f31899d5f354629eb8ca656b2f24740a6b72eaf3
Helmut Hummel <typo3@helhum.io>
2021-09-08 13:47:08 +0200

    [!!!][BUGFIX] Remove evaluation of Commands.php in Extensions
    This change should be considered as part of 7.0.0, but was lacking.
    Without it a fatal error occurs, because commands from TYPO3 Console
    are registered twice.
    
    Since TYPOO3 now can handle low level commands with TYPO3 11,
    the possibility to register them with TYPO3 Console has become
    obsolete and therefore is removed.
    
    Fixes: #1009

--- Classes/Console/Mvc/Cli/Typo3CommandRegistry.php
+++ Classes/Console/Mvc/Cli/Typo3CommandRegistry.php
@@ -15,8 +15,6 @@ namespace Helhum\Typo3Console\Mvc\Cli;
  */
 
 use TYPO3\CMS\Core\Console\CommandRegistry;
-use TYPO3\CMS\Core\Package\PackageManager;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Overrides the TYPO3 command registry to only contain the commands defined as services
@@ -32,48 +30,5 @@ class Typo3CommandRegistry extends CommandRegistry
     public function getServiceConfiguration(): array
     {
         return $this->commandConfigurations;
-    }
-
-    public function getCommandConfiguration(): array
-    {
-        // TODO: Fixme! This should be an empty array in 11, needs further conceptual steps
-        $packageManager = GeneralUtility::makeInstance(PackageManager::class);
-        $commandConfigurations = [];
-        foreach ($packageManager->getActivePackages() as $package) {
-            $commandsOfExtension = $package->getPackagePath() . 'Configuration/Commands.php';
-            if (!file_exists($commandsOfExtension)) {
-                continue;
-            }
-            if (!is_array($commands = require $commandsOfExtension)) {
-                continue;
-            }
-            $vendor = $package->getPackageKey();
-            foreach ($commands as $commandName => $commandConfig) {
-                if (!empty($this->commandConfigurations[$commandName])
-                    && $commandConfig['class'] === $this->commandConfigurations[$commandName]['serviceName']
-                ) {
-                    // Lazy (DI managed) commands override classic commands from Configuration/Commands.php
-                    // Skip this case to allow extensions to provide commands via DI config and to allow
-                    // TYPO3 v9 backwards compatible configuration via Configuration/Commands.php.
-                    // Note: Also the deprecation error is skipped on-demand as the extension has been
-                    // adapted and the configuration will be ignored as of TYPO3 v11.
-                    continue;
-                }
-                $commandConfig['vendor'] = $commandConfig['vendor'] ?? $vendor;
-                $commandConfig['name'] = $commandName;
-                $commandConfig['nameSpacedName'] = $commandConfig['vendor'] . ':' . $commandName;
-                $commandConfig['service'] = false;
-                $commandConfigurations[] = $commandConfig;
-
-                if (!isset($commandConfig['runLevel'])) {
-                    trigger_error(
-                        'Registering console commands in Configuration/Commands.php has been deprecated and will stop working in TYPO3 v11.0.',
-                        E_USER_DEPRECATED
-                    );
-                }
-            }
-        }
-
-        return $commandConfigurations;
     }
 }
