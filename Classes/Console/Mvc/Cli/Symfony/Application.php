faa96c02ed58c135fcd98b530f5aadeb5ff1ecf3
Helmut Hummel <typo3@helhum.io>
2022-04-11 11:55:08 +0200

    Release 6.7.2

--- Classes/Console/Mvc/Cli/Symfony/Application.php
+++ Classes/Console/Mvc/Cli/Symfony/Application.php
@@ -41,7 +41,7 @@ use TYPO3\CMS\Core\Information\Typo3Version;
  */
 class Application extends BaseApplication
 {
-    const TYPO3_CONSOLE_VERSION = '6.7.1';
+    const TYPO3_CONSOLE_VERSION = '6.7.2';
     const COMMAND_NAME = 'typo3cms';
 
     /**
